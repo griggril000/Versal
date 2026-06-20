593b4f6c94cadc2db2410825942cfc9a1cc59ab9
Riley Grigg <griggriley@gmail.com>
2026-06-20 12:23:49 -0600

    Removed unused imports

--- app/src/main/java/com/grigg/versal/MainActivity.kt
+++ app/src/main/java/com/grigg/versal/MainActivity.kt
@@ -4,12 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfoV2
 import androidx.compose.material3.adaptive.layout.PaneExpansionAnchor
